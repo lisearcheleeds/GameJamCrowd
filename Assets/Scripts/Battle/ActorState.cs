4fe9d82acd86bf741c6bbb0e247e7f2804225f88
LisetteLander <LisetteLander@gmail.com>
2020-08-17 14:20:13 +0900

    f

--- Assets/Scripts/Battle/ActorState.cs
+++ Assets/Scripts/Battle/ActorState.cs
@@ -17,11 +17,11 @@ public class ActorState
     public bool IsDead => HP <= 0;
 
     public int AttackInterval => (150 - Skills.Take(3).Count(x => x) * 30) / 2;
-    public float MoveSpeed => (0.03f + Skills.Take(3).Count(x => x) * 0.02f) * 2;
+    public float MoveSpeed => (0.03f + Skills.Take(3).Count(x => x) * 0.02f) * 5;
     public int HPMax => 50 + Skills.Skip(3).Take(3).Count(x => x) * 50;
-    public float HPRegeneration => (0.003f + Skills.Skip(3).Take(3).Count(x => x) * 0.002f) * 2;
+    public float HPRegeneration => (0.003f + Skills.Skip(3).Take(3).Count(x => x) * 0.002f) * 5;
     public int AttackDamage => 10 + Skills.Skip(6).Take(3).Count(x => x) * 5;
-    public float BulletSpeed => (0.15f + Skills.Skip(6).Take(3).Count(x => x) * 0.1f) * 2;
+    public float BulletSpeed => (0.15f + Skills.Skip(6).Take(3).Count(x => x) * 0.1f) * 5;
 
     public bool IsMoveAttack => Skills.Take(3).All(x => x);
     public bool IsDodge => Skills.Skip(3).Take(3).All(x => x);
